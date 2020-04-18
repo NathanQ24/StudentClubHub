9319de7b1ec61399d290d54cbfe145f3afcc08de
nhadley1 <58224546+nhadley1@users.noreply.github.com>
2020-04-18 18:18:32 -0400

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,23 @@
 import ClubTemp from './components/ClubTemp.vue'
 import Homepage from './components/Homepage.vue'
-import Login from './components/Login.vue'
+import BlogTemp from './components/BlogTemp.vue'
+// import AddBlog from './components/AddBlog.vue'
+import AddClub from './components/AddClub.vue'
 import Register from './components/Register.vue'
+import Login from './components/Login.vue'
+import SingleClub from './components/SingleClub.vue'
 import AddPost from './components/AddPost.vue'
+
+
 
 export default[
     {path: "/", component: Homepage},
     {path: "/club", component: ClubTemp},
-    {path: "/login", component: Login},
+    {path: "/blog", component: BlogTemp},
+    // {path: "/addBlog", component: AddBlog},
     {path: "/register", component: Register},
+    {path: "/addClub", component: AddClub},
+    {path: "/login", component: Login},
+    {path: "/club/:id", component: SingleClub},
     {path: "/add-post", component: AddPost}
 ]
